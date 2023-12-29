39487a08d64e538fb16e14d104e3c9d46bab545a
Shekhar2107 <shekharsuman0397@gmail.com>
2023-12-29 12:47:00 +0530

    first commit

--- vlcpp/Equalizer.hpp
+++ vlcpp/Equalizer.hpp
@@ -20,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#ifndef EQUALIZER_HPP
+#ifndef EQUALIZER_HPPd
 #define EQUALIZER_HPP
 
 #include "common.hpp"
